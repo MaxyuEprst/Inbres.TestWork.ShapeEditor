d74a513f0215a3d70ac4094b9c1dcea2bc533eb7
MaxyuEprst <num_one_id@vk.com>
2025-11-02 18:40:14 +0300

    изм структуры, и абстр. класс shape

---
+++ Source/Inbres.TestWork.ShapeEditor.Editor/Entities/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inbres.TestWork.ShapeEditor.Editor.Entities.Shapes
+{
+    public abstract class Shape
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = string.Empty;
+        public abstract ShapeType Type { get; }
+    }
+
+    public enum ShapeType
+    {
+        Oval,
+        BezierCurve
+    }
+}
