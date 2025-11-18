a48cc7a22fccddb4688a649c5394a3ba8ab58626
MaxyuEprst <num_one_id@vk.com>
2025-11-18 18:21:16 +0300

    минус лишнее

--- Source/Editor/Features/Saving/ShapeSerializer.cs
+++ Source/Editor/Features/Saving/ShapeSerializer.cs
@@ -12,30 +12,7 @@ namespace Editor.Features.Saving
         {
             WriteIndented = true,
             Converters = { new PointConverter() },
-            TypeInfoResolver = new DefaultJsonTypeInfoResolver
-            {
-                Modifiers = { EnablePolymorphism }
-            }
         };
-
-        private static void EnablePolymorphism(JsonTypeInfo typeInfo)
-        {
-            if (typeInfo.Type == typeof(EditorShape))
-            {
-                typeInfo.PolymorphismOptions = new JsonPolymorphismOptions
-                {
-                    TypeDiscriminatorPropertyName = "$type",
-                    IgnoreUnrecognizedTypeDiscriminators = true,
-                    UnknownDerivedTypeHandling = JsonUnknownDerivedTypeHandling.FailSerialization,
-                    DerivedTypes =
-                    {
-                        new JsonDerivedType(typeof(OvalShape), "oval"),
-                        new JsonDerivedType(typeof(BezCurShape), "bezier")
-                    }
-                };
-            }
-        }
-
 
         public string Serialize(IEnumerable<EditorShape> shapes)
         {
